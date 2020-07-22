963c5e6e10330d400704df58898a33771fbb6158
fgy <feng.hun@qq.com>
2020-07-22 23:01:36 +0800

    add .gitignore

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -3,10 +3,8 @@ package com.example;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = "com.example.*")
 @MapperScan("com.example.dao")
 public class DemoApplication {
 
